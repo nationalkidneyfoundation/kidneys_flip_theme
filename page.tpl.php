494774eb3caca737220281c405d8b95c7a453e31
Alan C <alanc@kidney.org>
2016-09-06 16:25:00 -0400

    added code to page tpl

---
+++ page.tpl.php
@@ -0,0 +1,57 @@
+<section class="bg--black padding-top--xxxl padding-x--lg">
+  <div class="container text-align--center">
+  	<img src="http://kidneyfoundation.cachefly.net/flip/FlipType3A.png" class="display--inline md--padding--xl" style="max-width: 600px" />
+  </div>
+</section>
+
+<section class="bg--black padding-top--lg padding-bottom--xxxl padding-x--lg color--gray-1">
+	<div class="container text-align--center">
+		<div class="grid md--width--85 lg--width--75 width--100 center">
+			<div class="grid-cell width--100 text-align--center font-weight--80 padding-bottom--xxxl">
+				<div class="font-weight--800 font-size--xxl">Coming Soon!</div>
+				<div class="font-size--lg">“86 the junk, I’ve only got one body to flunk<br> can’t leave my future behind<br> flip it – the choices are mine.”</div>
+			</div>
+		  <div class="bg--gray-3 grid-cell display--table width--100 padding--sm border-radius--md">
+		    <div class="display--table-cell width--70 border-color--aqua border-style--solid border-width--xs"><input placeholder="Enter your email" class="width--100" type="text"></div>
+      <a href="#" class="display--table-cell width--30 border-radius--none button--aqua padding-y--sm caps" type="submit" value="t"><span class="md--show">Get on the list</span><span class="md--hide">Notify Me</span></a></div>
+	
+	  </div>
+
+  	<div class="grid padding-top--xxxl">
+	    <div class="font-size--lg grid-cell width--100 text-align--center font-weight--800 padding-bottom--sm">
+	      <div>Know what’s safe to eat based on your own custom beat.</div>
+	    </div>
+	    <div class="padding--md grid-cell text-align--center sm--width--33 width--100">
+	      <div class=" padding-top--xs bg--black color--gray-1">
+	        <img class="width--100" src="http://kidneyfoundation.cachefly.net/flip/phife&rasta1.jpg">
+	        <p class="font-size--lg padding-x--md text-transform--uppercase">Phife Dawg & DJ Rasta Root<br /> A Tribe Called Quest</p>
+	      </div>
+	    </div>
+	    <div class="padding--md grid-cell text-align--center sm--width--33 width--100">
+	    	<div class=" padding-top--xs bg--black color--gray-1">
+	      	<img class="width--100" src="http://kidneyfoundation.cachefly.net/flip/Detour_Blackalicious.jpeg">
+					<p class="font-size--lg padding-x--md text-transform--uppercase">Gift of Gab<br /> Blackalicious</p>
+	      </div>
+	    </div>
+	    <div class="padding--md grid-cell text-align--center sm--width--33 width--100">
+	    	<div class=" padding-top--xs bg--black color--gray-1">
+	      	<img class="width--100" src="http://kidneyfoundation.cachefly.net/flip/black_rob.jpeg"/>
+					<p class=" font-size--lg padding-x--md text-transform--uppercase">Black Rob</p>
+	      </div>
+	    </div>
+		</div>
+  </div>
+</section>
+<section class="bg--gray-2 padding-y--xxxl padding-x--lg">
+	<div class="container text-align--center">
+		<!--<div class="font-size--lg width--100 text-align--center font-weight--800 padding-bottom--xl">
+			<div>In collaboration with the National Kidney Foundation.</div>
+		</div>-->
+  	<img class="display--inline" src="http://kidney.org/profiles/kidneys_distro/themes/custom/nkf_base/img/NKF-logoR_Hori_B.png"/>
+   	<div class="padding-top--xxl">
+    	<a href="#" class="margin-right--xs button--twitter"><i class="icon icon-twitter"></i> Twitter</a>
+    	<a href="#" class="margin-right--xs button--facebook"><i class="icon icon-facebook"></i> Facebook</a>
+      <a href="#" class="button--black"><i class="icon icon-instagram"></i> Instagram</a>
+   	</div>
+  </div>
+</section>
