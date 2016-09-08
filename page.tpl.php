0e0263b7043b3ff4a147ec6b71bbc6b8d9596916
Jacob Lane <jacoblane@Jacobs-MacBook-Pro.local>
2016-09-08 17:58:44 -0400

    https everywhere

--- page.tpl.php
+++ page.tpl.php
@@ -1,7 +1,7 @@
 <?php //if ($show_messages && $messages): print $messages; endif; ?>
 <section class="bg--black padding-top--xxxl padding-x--lg">
   <div class="container text-align--center">
-  	<img src="http://kidneyfoundation.cachefly.net/flip/FlipType3A.png" class="display--inline md--padding--lg padding--md" />
+  	<img src="https://kidneyfoundation.cachefly.net/flip/FlipType3A.png" class="display--inline md--padding--lg padding--md" />
   </div>
 </section>
 
@@ -22,19 +22,19 @@
 	    </div>
 	    <div class="padding--md grid-cell text-align--center sm--width--33 width--100">
 	      <div class=" padding-top--xs bg--black color--gray-1">
-	        <img class="width--100" src="http://kidneyfoundation.cachefly.net/flip/phife&rasta1.jpg">
+	        <img class="width--100" src="https://kidneyfoundation.cachefly.net/flip/phife&rasta1.jpg">
 	        <p class="font-size--lg padding-x--md text-transform--uppercase">Phife Dawg & DJ Rasta Root<br /> A Tribe Called Quest</p>
 	      </div>
 	    </div>
 	    <div class="padding--md grid-cell text-align--center sm--width--33 width--100">
 	    	<div class=" padding-top--xs bg--black color--gray-1">
-	      	<img class="width--100" src="http://kidneyfoundation.cachefly.net/flip/Detour_Blackalicious.jpeg">
+	      	<img class="width--100" src="https://kidneyfoundation.cachefly.net/flip/Detour_Blackalicious.jpeg">
 					<p class="font-size--lg padding-x--md text-transform--uppercase">Gift of Gab<br /> Blackalicious</p>
 	      </div>
 	    </div>
 	    <div class="padding--md grid-cell text-align--center sm--width--33 width--100">
 	    	<div class=" padding-top--xs bg--black color--gray-1">
-	      	<img class="width--100" src="http://kidneyfoundation.cachefly.net/flip/black_rob.jpeg"/>
+	      	<img class="width--100" src="https://kidneyfoundation.cachefly.net/flip/black_rob.jpeg"/>
 					<p class=" font-size--lg padding-x--md text-transform--uppercase">Black Rob</p>
 	      </div>
 	    </div>
@@ -46,7 +46,7 @@
 		<!--<div class="font-size--lg width--100 text-align--center font-weight--800 padding-bottom--xl">
 			<div>In collaboration with the National Kidney Foundation.</div>
 		</div>-->
-  	<img class="display--inline" src="http://kidney.org/profiles/kidneys_distro/themes/custom/nkf_base/img/NKF-logoR_Hori_B.png"/>
+  	<img class="display--inline" src="https://kidney.org/profiles/kidneys_distro/themes/custom/nkf_base/img/NKF-logoR_Hori_B.png"/>
    	<div class="padding-top--xxl">
     	<a href="https://twitter.com/nkf" class="margin-right--xs button--twitter"><i class="icon icon-twitter"></i> Twitter</a>
     	<a href="https://www.facebook.com/nationalkidneyfoundation" class="margin-right--xs button--facebook"><i class="icon icon-facebook"></i> Facebook</a>
